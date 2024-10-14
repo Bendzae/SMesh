526f40bf93405f0cafcec171894778834d7c81a8
Ben <info@bendz.dev>
2024-10-14 08:45:12 +0200

    reexport SecondaryMap

--- src/prelude.rs
+++ src/prelude.rs
@@ -2,3 +2,5 @@ pub use crate::smesh::{
     edit_operations::*, error::*, iterators::*, mesh_query::*, model::connectivity::*,
     model::mesh::*, model::mesh_elements::*, selection::*, topological_operations::*, util::*, *,
 };
+
+pub use slotmap::SecondaryMap;
